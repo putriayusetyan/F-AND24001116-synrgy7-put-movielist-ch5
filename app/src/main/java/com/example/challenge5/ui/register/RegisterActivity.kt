1eea34a6277a111058d95ac6a173e0b42755f47c
putri ayu ssetyaningrum <putriayusetyan@mail.com>
2024-06-15 01:44:18 +0700

    update

--- app/src/main/java/com/example/challenge5/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/challenge5/ui/register/RegisterActivity.kt
@@ -1,10 +1,11 @@
 package com.example.challenge5.ui.register
 
-import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.challenge5.R
 import com.example.challenge5.ui.main.MainActivity
@@ -15,7 +16,6 @@ import kotlinx.coroutines.launch
 class RegisterActivity : AppCompatActivity() {
     private lateinit var dataStoreManager: DataStoreManager
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -27,10 +27,14 @@ class RegisterActivity : AppCompatActivity() {
 
         registerButton.setOnClickListener {
             val username = usernameEditText.text.toString()
-            GlobalScope.launch {
-                dataStoreManager.saveUser(username, true)
-                startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
-                finish()
+            if (username.isNotEmpty()) {
+                GlobalScope.launch {
+                    dataStoreManager.saveUser(username, true)
+                    startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+                    finish()
+                }
+            } else {
+                Toast.makeText(this, "Please enter a username", Toast.LENGTH_SHORT).show()
             }
         }
     }
